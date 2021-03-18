9036c7333a05b333eeed080461af44b284a7efe2
Vitaliy Balabanov <tertiumnon@gmail.com>
2021-03-18 21:29:16 +0300

    add leetcode problem

---
+++ leetcode/plus-one.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+const plusOne = (digits) => {
+  const len = digits.length;
+  let addDigit = 1;
+  for (let i = len - 1; i >= 0; i -= 1) {
+    const currDigit = digits[i] + addDigit;
+    if (currDigit === 10) {
+      digits[i] = 0;
+      if (i === 0) {
+        digits.unshift(1);
+      }
+    } else {
+      digits[i] = currDigit;
+      addDigit = 0;
+      return digits;
+    }
+  }
+  return digits;
+};
+
+module.exports = plusOne;
