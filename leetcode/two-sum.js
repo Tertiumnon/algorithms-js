30437c85c9a03842e9ed19a65ff46173c3183837
Vitaliy Balabanov <tertiumnon@gmail.com>
2021-03-21 14:43:49 +0300

    add two sum

---
+++ leetcode/two-sum.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+const twoSum = (nums, target) => {
+  const res = [];
+  const len = nums.length;
+  for (let i = 0; i < len; i += 1) {
+    const item = nums[i];
+    for (let j = len - 1; j > i; j -= 1) {
+      const rItem = nums[j];
+      if (item + rItem === target) {
+        return [i, j];
+      }
+    }
+  }
+};
+
+module.exports = twoSum;
