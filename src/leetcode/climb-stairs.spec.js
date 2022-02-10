78f298a718b0c8bad916a0fca6af19e312650f50
Vitaliy Balabanov <tertiumnon@gmail.com>
2022-02-10 12:02:22 +0300

    change climb stairs test file

--- src/leetcode/climb-stairs.spec.js
+++ src/leetcode/climb-stairs.spec.js
@@ -1,10 +1,11 @@
-const climbStairs = require('../leetcode/climb-stairs');
+const climbStairs = require('./climb-stairs');
 
 describe('testing climbStairs', () => {
   it('case 1', () => {
     const res = climbStairs(2);
     expect(2).toEqual(res);
   });
+
   it('case 2', () => {
     const res = climbStairs(3);
     expect(3).toEqual(res);
