0dd2c303be2c7247885ce45c1d4e621410ce9a36
Vitalii Balabanov <tertiumnon@gmail.com>
2024-12-02 13:01:07 +0100

    add dominant index algorithm

--- src/leetcode/dominant-index.spec.ts
+++ src/leetcode/dominant-index.spec.ts
@@ -1,7 +1,32 @@
 import { expect, test } from "bun:test";
-import dominantIndex from './dominant-index';
+import dominantIndex from "./dominant-index";
 
-test('case 1', () => {
-  const result = dominantIndex();
-  expect(result).toEqual();
+test("case 1", () => {
+	const result = dominantIndex([3, 6, 1, 0]);
+	expect(result).toEqual(1);
+});
+
+test("case 2", () => {
+	const result = dominantIndex([1, 2, 3, 4]);
+	expect(result).toEqual(-1);
+});
+
+test("case 3", () => {
+	const result = dominantIndex([0, 0, 0, 1]);
+	expect(result).toEqual(3);
+});
+
+test("case 4", () => {
+	const result = dominantIndex([1, 0]);
+	expect(result).toEqual(0);
+});
+
+test("case 5", () => {
+	const result = dominantIndex([0, 2, 3, 0]);
+	expect(result).toEqual(-1);
+});
+
+test("case 5", () => {
+	const result = dominantIndex([0, 0, 3, 2]);
+	expect(result).toEqual(-1);
 });
--- src/leetcode/dominant-index.ts
+++ src/leetcode/dominant-index.ts
@@ -1,5 +1,23 @@
 const dominantIndex = (nums: number[]): number => {
-  return 0;
+	const numsLen = nums.length;
+	let largestNum = nums[0];
+	let largestNumI = 0;
+	let nonLargestNum = nums[1];
+	for (let i = 1; i < numsLen; i += 1) {
+		const numsItem = nums[i];
+		if (numsItem > nonLargestNum) nonLargestNum = numsItem;
+		if (numsItem > largestNum) {
+			nonLargestNum = largestNum;
+			largestNum = numsItem;
+			largestNumI = i;
+		}
+	}
+	if (
+		(largestNum > 0 && nonLargestNum === 0) ||
+		largestNum / nonLargestNum >= 2
+	)
+		return largestNumI;
+	return -1;
 };
 
 export default dominantIndex;
